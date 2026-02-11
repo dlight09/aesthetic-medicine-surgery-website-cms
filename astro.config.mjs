282fd535aec19e19e1a11d2b2759fa1867c4e001
Dali Brahem <mohamedali.brahem.1@esprit.tn>
2026-02-11 18:56:55 +0100

    chore: use VERCEL_URL for preview site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
-const site = process.env.SITE_URL || 'http://localhost:4321';
+const site =
+  process.env.SITE_URL ??
+  (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:4321');
 
 export default defineConfig({
   site,
